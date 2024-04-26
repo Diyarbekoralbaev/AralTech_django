dd08ca8495dce776a48228d0463d80f07a38f801
Diyarbek Oralbaev <diyarbekoralbaev@gmail.com>
2024-04-26 15:13:16 -0700

    debug=False

--- AralTech_portfolio/settings.py
+++ AralTech_portfolio/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$7=*&#l3s0qr*rr#5*gv)nnu%57%4=!bn0rf#^o8w(xd#g8dd4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
